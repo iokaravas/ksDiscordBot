df3e80149b87ff7dd223fdc576069bba536a5369
Ioannis Karavas <io.karavas@gmail.com>
2019-05-14 09:01:45 +0300

    Shows difference if it's only positive :P

--- ksDiscordBot.js
+++ ksDiscordBot.js
@@ -173,8 +173,8 @@ class ksDiscordBot {
         }
 
         return `__Kickstarter Campaign ${fundedPercentage}:__\n
-Pledged Total:  ${pledgeText}  :moneybag: *${pledgedDiff<=0?pledgedDiffText:''}* \n
-Backers:            ${backerText}  :scream: *${backersDiff<=0?backersDiffText:''}*\n
+Pledged Total:  ${pledgeText}  :moneybag: *${pledgedDiff>0?pledgedDiffText:''}* \n
+Backers:            ${backerText}  :scream: *${backersDiff>0?backersDiffText:''}*\n
 Comments:      ${commentText}  :scream_cat: \n
 *Last changed on: ${this.lastChangeTime}* (GMT+2)
 *Last checked on: ${timeDate}* (GMT+2)`
